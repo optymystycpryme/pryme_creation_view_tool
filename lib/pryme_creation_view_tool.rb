0056fb7facc30c88b6d6718f5135062996ba24e4
brian haltom <brianwh87@gmail.com>
2019-04-05 20:05:01 -0700

    Dev tool initial commit, update readme and gemspec

--- lib/pryme_creation_view_tool.rb
+++ lib/pryme_creation_view_tool.rb
@@ -2,6 +2,5 @@ require "pryme_creation_view_tool/version"
 require "pryme_creation_view_tool/renderer"
 
 module PrymeCreationViewTool
-  class Error < StandardError; end
   # Your code goes here...
 end
